24ed9437a250c09bfa221f065f822c6cd69ef061
Myat Thu Khaing <myatthu10302@gmail.com>
2025-10-05 12:06:31 +0900

    Add LikePost and UnLikePost Feature

--- routers/content.js
+++ routers/content.js
@@ -116,4 +116,33 @@ router.post("/comments", auth, async (req, res) => {
     res.json(comment)
 })
 
+router.post("/likes/posts/:id", auth, async (req, res) => {
+    const { id } = req.params;
+    const user = res.locals.user;
+
+    const like = await prisma.postLike.create({
+        data: {
+            postId: Number(id),
+            userId: Number(user.id),
+        }
+    })
+
+    res.json({ like })
+
+})
+
+router.delete("/unlike/posts/:id", auth, async (req, res) => {
+    const { id } = req.params;
+    const user = res.locals.user;
+
+    await prisma.postLike.deleteMany({
+        where: {
+            postId: Number(id),
+            userId: Number(user.id),
+        }
+    })
+
+    res.json({ msg: `Unlike post ${id}`})
+})
+
 module.exports = { contentRouter: router };
